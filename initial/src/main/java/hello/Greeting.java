958a4f3ae937ca78e328998abaf3da2282cd3ad3
mazeiomli <mazeiomli@gmail.com>
2018-09-09 22:11:47 -0400

    Initial Commit

---
+++ initial/src/main/java/hello/Greeting.java
@@ -0,0 +1,33 @@
+package hello;
+
+public class Greeting {
+
+    private String name;
+    private String size;
+    private String toppings;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+}
